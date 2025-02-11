25994530fe197fa01d4b8a2fa8d50f242420e6a4
Anu9969 <sharmaanurag9969@gmail.com>
2025-02-11 07:32:51 +0530

    configure Vite server to allow specific hosts

--- tgRPS/vite.config.ts
+++ tgRPS/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['0a56-2401-4900-1c63-4e9b-dd8-b5ce-ba88-fd28.ngrok-free.app']
+  }
 })
